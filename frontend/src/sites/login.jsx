8bfcae9c53a352ec478904eb07384b7ea4848e25
Amir Schnell <amir.schnell@gmail.com>
2021-01-28 14:33:09 +0100

    added functional login component

--- frontend/src/sites/login.jsx
+++ frontend/src/sites/login.jsx
@@ -1,9 +1,70 @@
-export default function Login() {
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { userService } from '../services/userService';
+
+export default function Login5() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    let history = useHistory();
+
+    userService.logout();
+
+    var handleSubmit = function (e) {
+        e.preventDefault();
+
+        setSubmitted(true);
+
+        // stop here if form is invalid
+        if (!(username && password)) {
+            return;
+        }
+
+        setLoading(true);
+        userService.login(username, password)
+            .then(
+                user => {
+                    const { from } = window.location.state || { from: { pathname: "/" } };
+                    history.push(from);
+                },
+                error => {
+                    setError(error);
+                    setLoading(false); 
+                }
+            );
+    }
 
     return (
-        <div>
-            <h2>{data.title}</h2>
-            {/* <ReactImageGallery items={images} showFullscreenButton={false} autoPlay={false} showPlayButton={false} /> */}
+        <div className="col-md-6 col-md-offset-3">
+            <h2>Login</h2>
+            <form name="form" onSubmit={handleSubmit}>
+                <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" name="username" value={username} onChange={e => setUsername(e.currentTarget.value)} />
+                    {submitted && !username &&
+                        <div className="help-block">Username is required</div>
+                    }
+                </div>
+                <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" name="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+                    {submitted && !password &&
+                        <div className="help-block">Password is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary" disabled={loading}>Login</button>
+                    {loading &&
+                        <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                    }
+                </div>
+                {error &&
+                    <div className={'alert alert-danger'}>{error}</div>
+                }
+            </form>
         </div>
     );
-}
+};
