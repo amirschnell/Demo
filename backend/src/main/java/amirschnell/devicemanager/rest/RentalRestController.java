20b40992efb2520e160199cbe6f1459442a062b3
Amir Schnell <amir.schnell@gmail.com>
2021-01-28 17:47:04 +0100

    fixed ug in RentalRestController

--- backend/src/main/java/amirschnell/devicemanager/rest/RentalRestController.java
+++ backend/src/main/java/amirschnell/devicemanager/rest/RentalRestController.java
@@ -1,8 +1,12 @@
 package amirschnell.devicemanager.rest;
 
 import amirschnell.devicemanager.model.Rental;
+import amirschnell.devicemanager.model.User;
 import amirschnell.devicemanager.repository.RentalRepository;
+import amirschnell.devicemanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class RentalRestController {
 
     private final RentalRepository rentalRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public RentalRestController(RentalRepository rentalRepository) {
+    public RentalRestController(RentalRepository rentalRepository, UserRepository userRepository) {
         this.rentalRepository = rentalRepository;
+        this.userRepository = userRepository;
     }
 
     @PostMapping
     private void addRental(@RequestBody Rental rental) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User u = userRepository.findByName(auth.getName());
+        rental.setUser(u);
         rentalRepository.save(rental);
     }
 }
