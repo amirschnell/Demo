a7f85272c4b04515b98e323df8522edc7e6f3bcc
Amir Schnell <amir.schnell@gmail.com>
2021-01-28 11:59:28 +0100

    made user implement UserDetails

--- backend/src/main/java/amirschnell/devicemanager/model/User.java
+++ backend/src/main/java/amirschnell/devicemanager/model/User.java
@@ -1,14 +1,18 @@
 package amirschnell.devicemanager.model;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     private long id;
@@ -19,4 +23,36 @@ public class User {
     @Column
     private String password;
 
+    @Column
+    private String type;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of((GrantedAuthority) () -> type);
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
